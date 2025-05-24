6632cd79ea742f65031ef41b759c5e6396f8ba2e
Limoshka8907 <admitriev729@gmail.com>
2025-05-24 12:39:55 +0300

    Добавьте файлы проекта.

---
+++ BufferEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+namespace EntityWindowsTool
+{
+    public class BufferEntity
+    {
+        private static string Text1 = "Словесное описание алгоритма\r\n1.\tПользователь заходит в систему\r\n2.\tАвторизация\r\n2.1.\tАвторизация в роли менеджера\r\n2.2.\tАвторизация в роли покупателя\r\n2.3.\tАвторизация в роли руководства\r\n2.4.\tАвторизация в роли физ. лица\r\n2.5.\tАвторизация в роли кладовщика\r\n3.\tОкно менеджера\r\n3.1.\tМенеджер просматривает информацию о менеджерах\r\n3.2.\tМенеджер просматривает информацию о пользователях\r\n3.3.\tПросматривать и изменять информацию своего профиля\r\n3.4.\tАдминистрировать заказы\r\n4.\tОкно покупателя\r\n4.1.\tПросмотр заказов\r\n4.2.\tПросматривать и изменять информацию своего профиля\r\n4.3.\tОставлять заказы на оптовую или розничную покупку\r\n5.\tВыход из системы\r\n\r\n\r\n";
+
+        public void GetText1()
+        {
+            string textToCopy = Text1;
+
+            if (!string.IsNullOrWhiteSpace(textToCopy))
+            {
+                try
+                {
+                    System.Windows.Forms.Clipboard.SetDataObject(textToCopy, true);
+                    Console.WriteLine("Текст скопирован в буфер обмена!", "Успех");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Не удалось скопировать текст. Ошибка: {ex.Message}", "Ошибка");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Пустой текст нельзя скопировать.", "Ошибка");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("EntityWindowsTool")]
+[assembly: AssemblyDescription("EntityWindowsTool is a versatile tool designed for integrating and automating interactions with Windows-based graphical windows and dialog boxes. It enables programmatic window management, automated data input, element detection and control, and supports advanced UI interaction scenarios. This tool is particularly useful for automation testing, GUI scripting, and developing systems that streamline interaction with Windows graphical interfaces.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EntityWindowsTool")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("44b32f50-42eb-4627-8105-1e694bfa2fd7")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
